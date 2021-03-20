01b6892423c3ee4db406cdd7763aaf6faab6156f
Papareddy <33577042+Papareddy@users.noreply.github.com>
2021-03-20 12:59:32 +0100

    Update Co-varying_genes.R

--- figurewise_scripts/Co-varying_genes.R
+++ figurewise_scripts/Co-varying_genes.R
@@ -1,6 +1,6 @@
 source('Eucledian_distance.R')
 
-genes = c('AT4G19020','AT5G14620')
+genes = c("AT5G49160","AT1G57820","AT1G66050","AT5G39550")
 no_weight = distances(genes, TPM)
 weight = distances(genes, TPM, weighted = TRUE)
 z_no_weight = distances(genes, zscores)
